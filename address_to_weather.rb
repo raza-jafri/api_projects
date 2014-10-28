be8d6d43496383900aaa677e565d8565823b0983
raza-jafri <arjafri@gmail.com>
2014-10-28 18:46:11 -0500

    Updated second portion of API hwk
    address_to_weather + coords_to_weather updated.

--- address_to_weather.rb
+++ address_to_weather.rb
@@ -13,10 +13,42 @@ puts "What is the address you would like to know the weather for?"
 the_address = gets.chomp
 url_safe_address = URI.encode(the_address)
 
-# Your code goes below.
+url_of_data_we_want = "http://maps.googleapis.com/maps/api/geocode/json?address="
+url = url_of_data_we_want + url_safe_address
+raw_data = open(url).read
+parsed_data = JSON.parse(raw_data)
+results = parsed_data["results"]
+first = results[0]
+geometry = first["geometry"]
+location = geometry["location"]
+lat = location["lat"]
+lng = location["lng"]
+
+the_latitude = lat
+the_longitude = lng
+
+url_of_data_want = "https://api.forecast.io/forecast/298307ef253aa87e5417597760d871fc/"
+url_2 = url_of_data_want + the_latitude.to_s + "," + the_latitude.to_s
+raw_data_2 = open(url_2).read
+parsed_data_2 = JSON.parse(raw_data_2)
+currently = parsed_data_2["currently"]
+temperature = currently["temperature"]
+the_temperature = temperature
+
+hourly = parsed_data_2["hourly"]
+data = hourly["data"]
+first = data[1]
+summary = first["summary"]
+the_hour_outlook = summary
+
+daily = parsed_data_2["daily"]
+data = daily["data"]
+first = data[1]
+summary = first["summary"]
+the_day_outlook = summary
 
 # Ultimately, we want the following line to work when uncommented:
 
-# puts "The current temperature at #{the_address} is #{the_temperature} degrees."
-# puts "The outlook for the next hour is: #{the_hour_outlook}"
-# puts "The outlook for the next day is: #{the_day_outlook}"
+puts "The current temperature at #{the_address} is #{the_temperature} degrees."
+puts "The outlook for the next hour is: #{the_hour_outlook}"
+puts "The outlook for the next day is: #{the_day_outlook}"
--- coords_to_weather.rb
+++ coords_to_weather.rb
@@ -19,8 +19,28 @@ the_longitude = gets.chomp
 #   address_to_coords.rb to read from a remote API and parse
 #   the results.
 
+url_of_data_want = "https://api.forecast.io/forecast/298307ef253aa87e5417597760d871fc/"
+url = url_of_data_want + the_latitude + "," + the_longitude
+raw_data = open(url).read
+parsed_data = JSON.parse(raw_data)
+currently = parsed_data["currently"]
+temperature = currently["temperature"]
+the_temperature = temperature
+
+hourly = parsed_data["hourly"]
+data = hourly["data"]
+first = data[1]
+summary = first["summary"]
+the_hour_outlook = summary
+
+daily = parsed_data["daily"]
+data = daily["data"]
+first = data[1]
+summary = first["summary"]
+the_day_outlook = summary
+
 # Ultimately, we want the following line to work when uncommented:
 
-# puts "The current temperature at #{the_latitude}, #{the_longitude} is #{the_temperature} degrees."
-# puts "The outlook for the next hour is: #{the_hour_outlook}"
-# puts "The outlook for the next day is: #{the_day_outlook}"
+puts "The current temperature at #{the_latitude}, #{the_longitude} is #{the_temperature} degrees."
+puts "The outlook for the next hour is: #{the_hour_outlook}"
+puts "The outlook for the next day is: #{the_day_outlook}"
